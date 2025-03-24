06197478ad077a8e3dc8f406c0fb28efe5b57128
lucas-s-g <lucas.salazar@usach.cl>
2025-03-24 02:00:04 -0300

    arreglo funcional de web scraping

--- utils/web_scraping/__init__.py
+++ utils/web_scraping/__init__.py
@@ -1,4 +1,4 @@
-import web_scraping.scraping_tasas as scraping_tasas
-import web_scraping.fred_interest_rates as fred_interest_rates
+import utils.web_scraping.scraping_tasas as scraping_tasas
+import utils.web_scraping.fred_interest_rates as fred_interest_rates
 
 __all__ = ["scraping_tasas", "fred_interest_rates"]
